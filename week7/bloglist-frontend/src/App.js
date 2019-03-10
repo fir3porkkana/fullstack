d53cd4e0e8ca17b4bd2423b209b4010b070c2bff
fir3porkkana <tiera.salmi@gmail.com>
2019-03-10 22:07:35 +0200

    troublen jälkeen tyylitkin ja refreshaus fiksattu

--- week7/bloglist-frontend/src/App.js
+++ week7/bloglist-frontend/src/App.js
@@ -18,7 +18,7 @@ const Menu = () => {
         blogs
       </Link>
       <Link to="/about" style={padding}>
-        about
+        about (users)
       </Link>
     </div>
   )
@@ -41,6 +41,9 @@ const About = () => {
 }
 
 const SingleBlog = ({ blog }) => {
+  if (blog === undefined) {
+    return null
+  }
   return (
     <div>
       <h1>{blog.title}</h1>
@@ -109,22 +112,6 @@ const App = () => {
     setBlogs(blogs.concat(createdBlog))
     notify(`a new blog ${createdBlog.title} by ${createdBlog.author} added`)
   }
-
-  // const likeBlog = async blog => {
-  //   const likedBlog = { ...blog, likes: blog.likes + 1 }
-  //   const updatedBlog = await blogService.update(likedBlog)
-  //   setBlogs(blogs.map(b => (b.id === blog.id ? updatedBlog : b)))
-  //   notify(`blog ${updatedBlog.title} by ${updatedBlog.author} liked!`)
-  // }
-
-  // const removeBlog = async blog => {
-  //   const ok = window.confirm(`remove blog ${blog.title} by ${blog.author}`)
-  //   if (ok) {
-  //     const updatedBlog = await blogService.remove(blog)
-  //     setBlogs(blogs.filter(b => b.id !== blog.id))
-  //     notify(`blog ${updatedBlog.title} by ${updatedBlog.author} removed!`)
-  //   }
-  // }
 
   if (user !== null) {
     return (
